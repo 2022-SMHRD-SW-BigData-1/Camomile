461897f0720f02f7fae2b13a99b9c27326fcc1f8
ChanHo01~ <chp9227@naver.com>
2022-10-27 16:30:11 +0900

    찬호

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,49 @@
 // JSX => JS랑 똑같은데 HTML 구문으로 작성이 가능한 확장자.
 
 import { useState, useEffect } from "react";
-import { Navigation } from "./components/navigation";
-import { Navigation2 } from "./components/navigation2";
-import { Header } from "./components/header";
-import { Features } from "./components/features";
-import { RecommendNearby } from "./components/recommendNearby";
-import { RecommendNew } from "./components/recommendNew";
-import { RecommendKeyword } from "./components/recommendKeyword";
-import { MoveToAdPage } from "./components/moveToAdPage";
+import {Route, Routes} from 'react-router-dom';
+import { Home } from "./components/home";
+import { ConsumerHeader } from "./components/consumerHeader";
+
 // import { Services } from "./components/services";
 // import { Gallery } from "./components/gallery";
 // import { Testimonials } from "./components/testimonials";
 // import { Team } from "./components/Team";
 // import { Contact } from "./components/contact";
-import JsonData from "./data/data.json";
+// import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
+import { Navigation } from "./components/navigation";
 
-export const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true,
-});
+// export const scroll = new SmoothScroll('a[href*="#"]', {
+//   speed: 1000,
+//   speedAsDuration: true,
+// });
 
 const App = () => {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
+  // const [landingPageData, setLandingPageData] = useState({});
+  // useEffect(() => {
+  //   setLandingPageData(JsonData);
+  // }, []);
 
   return (
-    <div>
-      <Navigation />
-      <Navigation2 />
-      <Header data={landingPageData.Header} />
-      <RecommendNearby />
-      <RecommendNew />
-      <RecommendKeyword />
-      <MoveToAdPage />
+    <>
+      {/* <Home/> */}
+
+      <ConsumerHeader></ConsumerHeader>
+      
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/nav" element={< Navigation/>} />
+      </Routes> 
       {/* <Features data={landingPageData.Features} /> */}
       {/* <Services data={landingPageData.Services} />
       <Gallery data={landingPageData.Gallery}/>
       <Testimonials data={landingPageData.Testimonials} />
       <Team data={landingPageData.Team} />
       <Contact data={landingPageData.Contact} /> */}
-    </div>
+    </>
   );
-};
+}
 
 export default App;
---
+++ src/components/consumerHeader.jsx
@@ -0,0 +1,7 @@
+export const ConsumerHeader = () => {
+  return (
+    <div id="ConsumerHeader" className="text-center">
+     <h1>상단메뉴바</h1>
+    </div>
+  );
+};
